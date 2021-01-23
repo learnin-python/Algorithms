81eb1c1ad31791a97396dc21d79206a6b9bf676d
Le Bon Bauma <baumabulondo@gmail.com>
2021-01-23 23:03:58 +0300

    Deleting Node Other than the Head

--- main.py
+++ main.py
@@ -163,6 +163,12 @@ class LinkedList:
             return
         prev = None
         while cur_node and cur_node.data != key:
+            prev = cur_node
+            cur_node = cur_node.next
+        if cur_node is None:
+            return
+        prev.next = cur_node.next
+        cur_node = None
 
 
 
