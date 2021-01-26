b572dbd16bcf9752e5c35e6f6d933327ee15e370
Le Bon Bauma <baumabulondo@gmail.com>
2021-01-26 22:36:47 +0300

    Setting main position as previous Node

--- main.py
+++ main.py
@@ -179,6 +179,13 @@ class LinkedList:
             cur_node = None
             return
 
+        prev = None
+        count = 0
+        while cur_node and count != pos:
+            prev = cur_node
+            cur_node = cur_node.next
+            count += 1
+
 
 
 
