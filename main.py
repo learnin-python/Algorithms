5e13df60f305d400f1f892be591863784a0dec29
Le Bon Bauma <baumabulondo@gmail.com>
2021-01-27 22:07:02 +0300

    Required positional argument

--- main.py
+++ main.py
@@ -228,6 +228,6 @@ llist.delete_node("E")
 
 llist.print_list()
 llist.insert_after_node(llist.head.next, "D")
-print(llist.len_iterative())
+# print(llist.len_iterative())
 # llist.print_list()
-
+print(llist.len_recursive(llist.head))
