36a95f23879f97aa15f51f65baab08acf89f2789
Le Bon Bauma <baumabulondo@gmail.com>
2021-02-04 21:40:04 +0000

    Swapping nodes

--- main.py
+++ main.py
@@ -271,4 +271,8 @@ llist.insert_after_node(llist.head.next, "D")
 # llist.print_list()
 print(llist.len_recursive(llist.head))
 
+print(('Original List'))
+llist.print_list()
 
+llist.swap_nodes("B", "C")
+print("Swapping nodes B and C that are not head nodes")
