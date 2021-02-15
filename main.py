8d594c7f3ba96122cf93970fe5b44665f26b8a10
Le Bon Bauma <baumabulondo@gmail.com>
2021-02-14 19:24:37 -0700

    Duplicates

--- main.py
+++ main.py
@@ -281,39 +281,38 @@ class LinkedList:
             return _reverse_recursive(cur, prev)
         self.head = _reverse_recursive(cur=self.head, prev=None)
 
-def merge_sorted(self, llist):
+    def merge_sorted(self, llist):
 
-    p = self.head
-    q = llist.head
-    s = None
+        p = self.head
+        q = llist.head
+        s = None
 
+        if not p:
+            return q
+        if not q:
+            return p
+
+        if p.data <= q.data:
+            s = p
+            p = s.next
+        else:
+            s = q
+            q = s.next
+        new_head = s
+    while p and q:
+        if p.data <= q.data:
+            s.next = p
+            s = p
+            q = s.next
+        else:
+            s.next = p
+            s = p
+            q = s.next
     if not p:
-        return q
+        s.next = q
     if not q:
-        return p
-
-    if p.data <= q.data:
-        s = p
-        p = s.next
-    else:
-        s = q
-        q = s.next
-    new_head = s
-while p and q:
-    if p.data <= q.data:
         s.next = p
-        s = p
-        q = s.next
-    else:
-        s.next = p
-        s = p
-        q = s.next
-if not p:
-    s.next = q
-if not q:
-    s.next = p
-
-self.head = new_head
+    self.head = new_head
     return self.head
 
 # Removing Duplicates
