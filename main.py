f5b3ddf16144024ed85cf964fe19adadcf3f9814
Le Bon Bauma <baumabulondo@gmail.com>
2021-02-18 22:11:12 +0000

    Duplicates

--- main.py
+++ main.py
@@ -161,7 +161,7 @@ class LinkedList:
 
         new_node.next = self.head
         self.head =new_node
-#### To investigate further
+#### To investigate furthe
 # Inserting after a node
 
     def insert_after_node(self, prev_node, data):
