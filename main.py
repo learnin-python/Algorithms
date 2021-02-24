1620309c7a2f9bcec207b63f90a0ff2569cf00c6
Le Bon Bauma <baumabulondo@gmail.com>
2021-02-23 21:29:40 -0600

    Duplicates

--- main.py
+++ main.py
@@ -352,7 +352,7 @@ class LinkedList:
                 prev.next = cur.next
                 cur = None
             else:
-                # Have not encountered ele
+                # Have not encountered element
 
 
 
