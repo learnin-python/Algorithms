6614e6cce6b718a6fd6968b125d6c215f5ef755c
Le Bon Bauma <baumabulondo@gmail.com>
2021-02-24 22:53:55 -0600

    Duplicates

--- main.py
+++ main.py
@@ -381,7 +381,7 @@ print(llist.len_recursive(llist.head))
 print("The length of the linked list calculated iteratively after inserting 4 elements is:")
 print(llist.len_recursive())
 llist.delete_node("B")
-llist.delete_node
+llist.delete_nod
 
 llist.print_list()
 llist.insert_after_node(llist.head.next, "D")
