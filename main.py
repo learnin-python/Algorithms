370aaf8ee99b7614e8a58271a0ffb44402f55536
Le Bon Bauma <baumabulondo@gmail.com>
2021-02-26 16:36:29 -0600

    Duplicates

--- main.py
+++ main.py
@@ -353,7 +353,7 @@ class LinkedList:
                 cur = None
             else:
                 # Have not encountered element before.
-                dup_values
+                dup_values[cu]
 
 
 
