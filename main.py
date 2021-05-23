6d0f9ecf4013e2c33db7c7e683f5a208f386cb3a
Le Bon Bauma <baumabulondo@gmail.com>
2021-05-22 21:42:37 -0500

    Refactoring

--- main.py
+++ main.py
@@ -395,7 +395,6 @@ llist.delete_node("E")
 llist.print_list()
 llist.insert_after_node(llist.head.next, "D")
 # print(llist.len_iterative())
-
 # llist.print_list()
 print(llist.len_recursive(llist.head))
 
